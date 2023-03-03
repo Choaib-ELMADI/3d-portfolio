3921361108db07dad682538805d232d7b29fb41e
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-03-03 21:54:23 +0100

    Implementing Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
+import { ComputersCanvas } from './canvas'
+
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='app__hero'>
+      <div className='app__hero-text'>
+        <div className='app__hero-decoration'>
+          <div className="app__hero-deco-circle" />
+          <div className="app__hero-deco-line" />
+        </div>
+        <div className="app__main-text">
+          <h1>Hi, I'm <span>Choaib</span></h1>
+          <p>I develop 3D visuals, user <br />interfaces, and web applications.</p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
   )
 }
 
