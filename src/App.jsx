577f0efc176d45ea2570d20de43c24c94f322a35
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-03-03 19:19:18 +0100

    App layout implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <div className='app__nav'>
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className='app__stars'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
