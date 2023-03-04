a187a06ffe46fd562d5f667ceecb7ecac9091450
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-03-04 18:59:56 +0100

    Ball technologies implemented

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BallCanvas } from './canvas'
+import { SectionWrapper } from '../hoc/index'
+import { technologies } from '../constants'
+
+import './Tech.css'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='app__techs'>
+      {
+        technologies.map((tech) => (
+          <div className='app__tech' key={ tech.name }>
+            <BallCanvas icon={ tech.icon } />
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
