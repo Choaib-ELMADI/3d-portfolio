efd358198e32ffcae4bc65b113194b4ecb1fc3ec
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-03-04 12:32:51 +0100

    About done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,8 @@ import { motion } from 'framer-motion'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+import { SectionWrapper } from '../hoc/index'
+
 import './About.css'
 
 const ServiceCard = ({ index, title, icon }) => {
@@ -63,4 +65,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
